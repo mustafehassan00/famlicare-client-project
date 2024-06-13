1b5ceb2516ab9851fa62731d0571c3aacf099d2e
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-06-13 14:58:17 -0500

    add pgsql terminal creation

---
+++ database_create_tables.sql
@@ -0,0 +1,25 @@
+/**
+ * This script is used to build the database locally. To use it, follow these steps:
+ * 1. Open the Terminal.
+ * 2. Replace 'your_username' with your PostgreSQL username.
+ * 3. Run the following command:
+ *    psql -U your_username -d your_database_name -f database.sql
+ * 
+ * This command will execute the 'database.sql' script and create the database using the specified username and name.
+ */
+
+ 
+-- to build the FamiliCare database locally, Open the Terminal and run this command:
+
+psql -U your_username -d familicare -f database.sql
+
+-- Create Loved Ones table
+\ir ./database/LovedOnes.sql
+-- Create users table
+\ir ./database/Users.sql
+-- Create message table
+\ir ./database/Messages.sql
+-- Create vault table
+\ir ./database/Vault.sql
+--Create invitations table
+\ir ./database/Invitations.sql
