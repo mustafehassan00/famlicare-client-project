10aa75fa83bf0582ce9384200621856ee0e98f81
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-06-14 10:05:28 -0500

    rename User Table to allow prebuilt functions to work

--- database/Messages.sql
+++ database/Messages.sql
@@ -1,6 +1,6 @@
 CREATE TABLE messages(
     id SERIAL PRIMARY KEY,
     loved_one_id INTEGER REFERENCES loved_ones(id),
-    user_id INTEGER REFERENCES users(id),
+    user_id INTEGER REFERENCES "user"(id),
     message_text TEXT
 );
--- database/Users.sql
+++ database/Users.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users(
+CREATE TABLE "user"(
     id SERIAL PRIMARY KEY,
     username VARCHAR(100) UNIQUE NOT NULL,
     first_name VARCHAR(100),
