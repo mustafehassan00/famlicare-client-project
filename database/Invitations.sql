459b2d4703ee26d5e405e89d15df59bd17c2dba0
Janet Scanlon <janet.Lscanlon@gmail.com>
2024-06-14 09:52:05 -0500

    database syntax fixes

--- database/Invitations.sql
+++ database/Invitations.sql
@@ -3,5 +3,5 @@ CREATE TABLE invitations(
     invitation_code UUID DEFAULT gen_random_uuid(),
     email VARCHAR(255) NOT NULL CHECK (email ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'),
     permission_level VARCHAR(50) DEFAULT 'standard',
-    loved_one_id INTEGER REFERENCES loved_ones(id),
+    loved_one_id INTEGER REFERENCES loved_ones(id)
 );
