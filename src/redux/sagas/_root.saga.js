ca2f9beaea071ba09b639969d6e708e58d015fd4
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-06-14 11:56:39 -0500

    Added boiler sagas - Zeyini

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -2,6 +2,10 @@ import { all } from 'redux-saga/effects';
 import loginSaga from './login.saga';
 import registrationSaga from './registration.saga';
 import userSaga from './user.saga';
+import careTeamSaga from './careTeam.saga';
+import careVaultSaga from './careVault.saga';
+import lovedOneSaga from './lovedOne.saga';
+import messagesSaga from './messages.saga';
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
@@ -15,5 +19,9 @@ export default function* rootSaga() {
     loginSaga(), // login saga is now registered
     registrationSaga(),
     userSaga(),
+    careTeamSaga(),
+    careVaultSaga(),
+    lovedOneSaga(),
+    messagesSaga(),
   ]);
 }
