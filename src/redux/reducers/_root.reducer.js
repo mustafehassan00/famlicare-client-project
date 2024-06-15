fe51f33f121a2fdb70e20ec8d09b23bb14e0eb82
Zeyini Mohammad <Zeyinim@outlook.com>
2024-06-14 20:49:23 -0500

    commented out the reducers

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -15,10 +15,10 @@ import messages from './messages.reducer';
 const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   user, // will have an id and username if someone is logged in
-  careTeam,
-  careVault,
-  lovedOne,
-  messages,
+  // careTeam,
+  // careVault,
+  // lovedOne,
+  // messages,
 });
 
 export default rootReducer;
--- src/redux/reducers/careTeam.reducer.js
+++ src/redux/reducers/careTeam.reducer.js
@@ -1,14 +1,15 @@
 const careTeam = (state = {}, action) => {
-    // switch (action.type) {
-    //   case 'SET_USER':
-    //     return action.payload;
-    //   case 'UNSET_USER':
-    //     return {};
-    //   default:
-    //     return state;
-    // }
-  };
-  
-  // user will be on the redux state at:
-  // state.user
+  // switch (action.type) {
+  //   case 'SET_USER':
+  //     return action.payload;
+  //   case 'UNSET_USER':
+  //     return {};
+  //   default:
+  //     return state;
+  // }
+};
+
+// user will be on the redux state at:
+// state.user
+
   export default careTeam;
