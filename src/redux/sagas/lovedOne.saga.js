1f292953e571a9773233aeb89cd77aa8d6b1b5c9
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-06-15 22:57:31 -0500

    updating actions and saga

--- src/redux/sagas/lovedOne.saga.js
+++ src/redux/sagas/lovedOne.saga.js
@@ -1,36 +1,76 @@
 import { call, put, takeLatest } from 'redux-saga/effects';
-import { CREATE_LOVED_ONE_REQUEST, UPDATE_LOVED_ONE_REQUEST, UPDATE_USER_TABLE_REQUEST } from './actions';
-import { createLovedOneApi, updateLovedOneApi, updateUserTableApi } from './api'; // Define these API functions
+import {
+  CREATE_LOVED_ONE_REQUEST,
+  UPDATE_LOVED_ONE_REQUEST,
+  FETCH_LOVED_ONE_REQUEST,
+  DELETE_LOVED_ONE_REQUEST,
+  CREATE_LOVED_ONE_SUCCESS,
+  CREATE_LOVED_ONE_FAILURE,
+  UPDATE_LOVED_ONE_SUCCESS,
+  UPDATE_LOVED_ONE_FAILURE,
+  FETCH_LOVED_ONE_SUCCESS,
+  FETCH_LOVED_ONE_FAILURE,
+  DELETE_LOVED_ONE_SUCCESS,
+  DELETE_LOVED_ONE_FAILURE,
+  AUTHORIZATION_FAILURE,
+} from '../reducers/actions/lovedOne.actions.js';
+import {
+  createLovedOneApi,
+  updateLovedOneApi,
+  fetchLovedOneApi,
+  deleteLovedOneApi,
+} from '../api'; // Assuming these API functions are defined elsewhere
+
+// Centralized error handling
+function* handleError(error, failureAction) {
+  console.error('Saga error:', error);
+  if (error.status === 401) {
+    yield put({ type: AUTHORIZATION_FAILURE, error });
+  } else {
+    yield put({ type: failureAction, error });
+  }
+}
 
 function* createLovedOneSaga(action) {
   try {
     const response = yield call(createLovedOneApi, action.payload);
-    yield put({ type: 'CREATE_LOVED_ONE_SUCCESS', payload: response });
+    yield put({ type: CREATE_LOVED_ONE_SUCCESS, response });
   } catch (error) {
-    yield put({ type: 'CREATE_LOVED_ONE_FAILURE', error });
+    yield* handleError(error, CREATE_LOVED_ONE_FAILURE);
   }
 }
 
 function* updateLovedOneSaga(action) {
   try {
-    const response = yield call(updateLovedOneApi, action.payload.id, action.payload.data);
-    yield put({ type: 'UPDATE_LOVED_ONE_SUCCESS', payload: response });
+    const response = yield call(updateLovedOneApi, action.payload);
+    yield put({ type: UPDATE_LOVED_ONE_SUCCESS, response });
   } catch (error) {
-    yield put({ type: 'UPDATE_LOVED_ONE_FAILURE', error });
+    yield* handleError(error, UPDATE_LOVED_ONE_FAILURE);
   }
 }
 
-function* updateUserTableSaga(action) {
+function* fetchLovedOneSaga(action) {
   try {
-    const response = yield call(updateUserTableApi, action.payload);
-    yield put({ type: 'UPDATE_USER_TABLE_SUCCESS', payload: response });
+    const response = yield call(fetchLovedOneApi, action.payload);
+    yield put({ type: FETCH_LOVED_ONE_SUCCESS, response });
   } catch (error) {
-    yield put({ type: 'UPDATE_USER_TABLE_FAILURE', error });
+    yield* handleError(error, FETCH_LOVED_ONE_FAILURE);
   }
 }
 
+function* deleteLovedOneSaga(action) {
+  try {
+    const response = yield call(deleteLovedOneApi, action.payload);
+    yield put({ type: DELETE_LOVED_ONE_SUCCESS, response });
+  } catch (error) {
+    yield* handleError(error, DELETE_LOVED_ONE_FAILURE);
+  }
+}
+
+// Root saga
 export default function* rootSaga() {
   yield takeLatest(CREATE_LOVED_ONE_REQUEST, createLovedOneSaga);
   yield takeLatest(UPDATE_LOVED_ONE_REQUEST, updateLovedOneSaga);
-  yield takeLatest(UPDATE_USER_TABLE_REQUEST, updateUserTableSaga);
+  yield takeLatest(FETCH_LOVED_ONE_REQUEST, fetchLovedOneSaga);
+  yield takeLatest(DELETE_LOVED_ONE_REQUEST, deleteLovedOneSaga);
 }
