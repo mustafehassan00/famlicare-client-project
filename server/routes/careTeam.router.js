cc03cf9169a5a9fd0e0b1cb46e90500be8de473f
Janet Scanlon <janet.Lscanlon@gmail.com>
2024-06-17 11:26:07 -0500

    created dispatch in CareTeamRegistration component for POST Route for invited users

--- server/routes/careTeam.router.js
+++ server/routes/careTeam.router.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+require('dotenv').config()
+
+const sgMail = require('@sendgrid/mail')
+const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY
+
+sgMail.setApiKey(SENDGRID_API_KEY)
+
 
 /**
  * GET route template
@@ -9,11 +16,20 @@ router.get('/', (req, res) => {
   // GET route code here
 });
 
-/**
- * POST route template
- */
+
 router.post('/', (req, res) => {
-  // POST route code here
+
+    const email = {
+      to: `${req.body.email}`,
+      from: 'famlicareappclientproject@gmail.com',
+      subject: 'Test SendGrid Email',
+      text: 'Welcome to FamliCare',
+      html: '<h1>Welcome to FamliCare</h1>',
+  }
+
+  sgMail.send(email)
+      .then((response) => console.log('Email Sent Successfully!'))
+      .catch((error) => console.log(error.email))
 });
 
 module.exports = router;
--- src/components/CareTeamForm/CareTeamForm.jsx
+++ src/components/CareTeamForm/CareTeamForm.jsx
@@ -1,35 +1,40 @@
-import React from "react";
-import  { sgMail } from "@sendgrid/mail"
-import dotenv from "dotenv";
+import React, {useState} from "react";
+import { useDispatch, useSelector } from 'react-redux'
+
 
 //import MUI components
 import { Button, TextField } from "@mui/material";
 
 
 function CareTeamForm() {
-    //sgMail.setApiKey(process.env.SENDGRID_API_KEY) 
+    
+    //Declare new state for the invitedUsers Email 
+    const [invitedUserEmailInput, setInvitedUserEmailInput] = useState('')
+
+    const dispatch = useDispatch()
 
     const sendEmail = () =>  {
-
-        // const email = {
-        //     to: 'famlicareappclientproject@gmail.com',
-        //     from: 'famlicareappclientproject@gmail.com',
-        //     subject: 'Test SendGrid Email',
-        //     text: 'Welcome to FamliCare',
-        //     html: '<h1>Welcome to FamliCare</h1>',
-        // }
-
-        // sgMail.send(email)
-        //     .then((response) => console.log('Email Sent Successfully!'))
-        //     .catch((error) => console.log(error.email))
+        //onClick send a dispatch to invitee invited users table 
+        console.log('send email clicked! email is:', invitedUserEmailInput)
+        dispatch({
+            type: 'SEND_EMAIL_TO_INVITED_USER',
+            payload: {
+                email:invitedUserEmailInput,
+            }
+        })
+        setInvitedUserEmailInput('')
+       
     }
 
 
     return(
         <div>
             <h1>Create a CareTeam</h1>
-            <TextField label='Email' variant="outlined"/>
-            <Button onClick={sendEmail}>Send Email!</Button>
+            <TextField 
+                onChange={(e) => setInvitedUserEmailInput(e.target.value)}
+                label='Email' 
+                variant="outlined"/>
+            <Button onClick={sendEmail}>Send Email</Button>
         </div>
     )
 }
