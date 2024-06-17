e4f524107bf9037ecc9cfaf868c98e037cdbcd3d
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-06-17 10:10:35 -0500

    enable user reducer

--- src/redux/reducers/careTeam.reducer.js
+++ src/redux/reducers/careTeam.reducer.js
@@ -1,10 +1,10 @@
 const careTeam = (state = {}, action) => {
-    // switch (action.type) {
-    //   case 'SET_USER':
-    //     return action.payload;
-    //   case 'UNSET_USER':
-    //     return {};
-    //   default:
+    switch (action.type) {
+      case 'SET_USER':
+        return action.payload;
+      case 'UNSET_USER':
+        return {};
+      default:
         return state;
     };
   
