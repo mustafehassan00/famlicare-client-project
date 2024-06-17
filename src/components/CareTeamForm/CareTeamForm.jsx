1fe82c7b6313fda3ddad401465cf9a17cbb8ccd0
Janet Scanlon <janet.Lscanlon@gmail.com>
2024-06-16 21:33:07 -0500

    getting 'cannot read properties of undefined' error when trying to set the api key

--- src/components/CareTeamForm/CareTeamForm.jsx
+++ src/components/CareTeamForm/CareTeamForm.jsx
@@ -1,11 +1,33 @@
 import React from "react";
+import  { sgMail } from "@sendgrid/mail"
+
+
 
 function CareTeamForm() {
+
+   
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY) 
+
+    const sendEmail = () =>  {
+
+        const email = {
+            to: 'famlicareappclientproject@gmail.com',
+            from: 'famlicareappclientproject@gmail.com',
+            subject: 'Test SendGrid Email',
+            text: 'Welcome to FamliCare',
+            html: '<h1>Welcome to FamliCare</h1>',
+        }
+
+        sgMail.send(email)
+            .then((response) => console.log('Email Sent Successfully!'))
+            .catch((error) => console.log(error.email))
+    }
 
 
     return(
         <div>
             <h1>Create a CareTeam</h1>
+            <button onClick={sendEmail}>Send Email!</button>
         </div>
     )
 }
