651c6cdb5cca9dc94b9ae706386c178ffcdeef29
Janet Scanlon <janet.Lscanlon@gmail.com>
2024-06-17 10:32:24 -0500

    installed MUI

--- src/components/CareTeamForm/CareTeamForm.jsx
+++ src/components/CareTeamForm/CareTeamForm.jsx
@@ -2,31 +2,34 @@ import React from "react";
 import  { sgMail } from "@sendgrid/mail"
 import dotenv from "dotenv";
 
+//import MUI components
+import { Button, TextField } from "@mui/material";
 
 
 function CareTeamForm() {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY) 
+    //sgMail.setApiKey(process.env.SENDGRID_API_KEY) 
 
     const sendEmail = () =>  {
 
-        const email = {
-            to: 'famlicareappclientproject@gmail.com',
-            from: 'famlicareappclientproject@gmail.com',
-            subject: 'Test SendGrid Email',
-            text: 'Welcome to FamliCare',
-            html: '<h1>Welcome to FamliCare</h1>',
-        }
+        // const email = {
+        //     to: 'famlicareappclientproject@gmail.com',
+        //     from: 'famlicareappclientproject@gmail.com',
+        //     subject: 'Test SendGrid Email',
+        //     text: 'Welcome to FamliCare',
+        //     html: '<h1>Welcome to FamliCare</h1>',
+        // }
 
-        sgMail.send(email)
-            .then((response) => console.log('Email Sent Successfully!'))
-            .catch((error) => console.log(error.email))
+        // sgMail.send(email)
+        //     .then((response) => console.log('Email Sent Successfully!'))
+        //     .catch((error) => console.log(error.email))
     }
 
 
     return(
         <div>
             <h1>Create a CareTeam</h1>
-            <button onClick={sendEmail}>Send Email!</button>
+            <TextField label='Email' variant="outlined"/>
+            <Button onClick={sendEmail}>Send Email!</Button>
         </div>
     )
 }
