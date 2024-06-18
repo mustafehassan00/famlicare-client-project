3a085ddbff45dbe52dc7eb2efa3e925990e8bf5e
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-06-18 09:11:45 -0500

    add link to loved one form

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -32,6 +32,10 @@ function Nav() {
               Info Page
             </Link>
 
+            <Link className="navLink" to="/lovedoneform">
+              Loved one form
+            </Link>
+
             <Link className="navLink" to="/careteamform">
               CareTeamForm
             </Link>
