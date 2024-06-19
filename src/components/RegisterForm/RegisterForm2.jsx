23d940a0049fbb57d0a0a26efb54780d756c472d
Zeyini Mohammad <Zeyinim@outlook.com>
2024-06-19 16:35:04 -0500

    Declared from data to hold  the uploaded photo by the user

--- src/components/RegisterForm/RegisterForm2.jsx
+++ src/components/RegisterForm/RegisterForm2.jsx
@@ -1,40 +1,28 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
-import axios from "axios";
+
 
 function RegisterForm2() {
   const [selectedFile, setSelectedFile] = useState(null); // State to hold selected file
   const history = useHistory();
 
   const handleFileChange = (event) => {
-    setSelectedFile(event.target.files[0]); // Capture the selected file
+    setSelectedFile(event.target.files[0]); // Capture the selected file and save it to selectedFile variable
   };
 
   const continueRegistration = async () => {
+    // //if a file is not selected alert to select a file.
     if (!selectedFile) {
       alert("Please select a file.");
       return;
     }
 
     try {
-      const formData = new FormData();
-      formData.append("photo", selectedFile); // Append the file to FormData
-
-      const config = {
-        headers: {
-          "content-type": "multipart/form-data"
-        }
-      };
-
-      const response = await axios.post("/api/user", formData, config);
-      console.log("File uploaded successfully:", response.data);
+    
       // Dispatch action or handle state update if needed
       history.push("/registerpage/registerpage3");
-    } catch (error) {
-      console.error("Error uploading file:", error);
-      // Handle error if needed
-    }
+    } 
   };
 
   return (
