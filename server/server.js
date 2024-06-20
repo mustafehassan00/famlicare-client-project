85281779ac5776eaa7802be3c1e0a6561d275a94
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-06-19 20:53:37 -0500

    correcting imports

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 5001;
+const path = require('path');
 
 // Middleware Includes
 const sessionMiddleware = require('./modules/session-middleware');
