e4377a88970a09ced5cd5d3f5fd798265ba80b1a
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-06-20 09:59:50 -0500

    update user page styling

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Container, Typography, Button, useTheme } from '@mui/material';
 import LogOutButton from '../LogOutButton/LogOutButton';
-import {useSelector} from 'react-redux';
 
 function UserPage() {
-  // this component doesn't do much to start, just renders some user reducer info to the DOM
+  const theme = useTheme();
   const user = useSelector((store) => store.user);
+
   return (
-    <div className="container">
-      <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
-      <LogOutButton className="btn" />
-    </div>
+    <Container sx={{ marginTop: theme.spacing(4) }}>
+      <Typography 
+        variant="h2" 
+        sx={{ marginBottom: theme.spacing(2) }}
+      >
+        Welcome, {user.username}!
+      </Typography>
+      <Typography variant="body1">
+        Your ID is: {user.id}
+      </Typography>
+      {/* Assuming LogOutButton can accept MUI Button props */}
+      <LogOutButton 
+        className="btn" 
+        component={Button} 
+        sx={{ marginTop: theme.spacing(2) }}
+      />
+    </Container>
   );
 }
 
-// this allows us to use <App /> in index.js
 export default UserPage;
