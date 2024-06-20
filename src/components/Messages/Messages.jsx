ebe2f3e02d1a8403f0ce51c4c2fdd2597cd35234
Mustafe Hassan <mustafeh123@gmail.com>
2024-06-20 16:07:22 -0500

    Messages is now able to auto populate the roomID and Username

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -11,17 +11,29 @@ const socket = io.connect("http://localhost:5173");
 
 
 function Messages() {
-    // Use this to use as auto fill to fill in the user 
+// Use this to use as auto fill to fill in the user 
 const user = useSelector((store) => store.user);
 const firstName = user.first_name
 const lastName = user.last_name
 const usernameID = user.username
-const fullName = usernameID
+const fullName = firstName + lastName;
 
 const [username, setUsername] = useState(fullName); // Initialize with fullName
+
 const handleUsernameChange = (e) => {
     setUsername(e.target.value);
 };
+
+// Loved One data ID will be used to auto populate for the roomID 
+const lovedOneID = user.loved_one_id;
+const [lovedOne, newLovedOne] = useState(lovedOneID);
+
+const handleRoomIDChange = (e) => {
+    newLovedOne(e.target.value);
+}
+
+
+
     return(
         <div>
             <h4>Enter Loved One's Name</h4>
@@ -36,6 +48,8 @@ const handleUsernameChange = (e) => {
             <input 
             id = "RoomId"
             type="text" 
+            value = {lovedOne}
+            onChange={handleRoomIDChange}
              />
             <button>Join Room</button>
         </div>
