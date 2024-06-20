094750e21242adc70032847d34aea58d85017992
Mustafe Hassan <mustafeh123@gmail.com>
2024-06-20 15:29:50 -0500

    populated the username with data

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -8,9 +8,37 @@ import { io } from 'socket.io-client';
 
 const socket = io.connect("http://localhost:5173");
 
+
+
 function Messages() {
+    // Use this to use as auto fill to fill in the user 
+const user = useSelector((store) => store.user);
+const firstName = user.first_name
+const lastName = user.last_name
+const usernameID = user.username
+const fullName = usernameID
+
+const [username, setUsername] = useState(fullName); // Initialize with fullName
+const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+};
     return(
-        <div></div>
+        <div>
+            <h4>Enter Loved One's Name</h4>
+            <input 
+            id = "Username"
+            type="text" 
+            value = {username}
+            onChange={handleUsernameChange}
+             />
+            
+            
+            <input 
+            id = "RoomId"
+            type="text" 
+             />
+            <button>Join Room</button>
+        </div>
     )
 }
 
