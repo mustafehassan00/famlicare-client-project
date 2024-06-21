9079eb5b97613afae93e98ff7c3cafcfd163f51f
Zeyini Mohammad <Zeyinim@outlook.com>
2024-06-21 12:28:42 -0500

    added an avatar for the profile image.

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Container, Typography, Button, useTheme,Grid } from '@mui/material';
+import { Container, Typography, Button, useTheme,Avatar } from '@mui/material';
 import LogOutButton from '../LogOutButton/LogOutButton';
 
 
@@ -8,10 +8,15 @@ function UserPage() {
   const theme = useTheme();
   const user = useSelector((store) => store.user);
 
+  const editUserinfo = () => {
+    // route to the form where the user can edit their information
+
+  };
+
   return (
     <>
     <Container sx={{ marginTop: theme.spacing(4),border: 2,
-          borderColor: theme.palette.primary.main }}>
+          borderColor: theme.palette.primary.light,alignItems: 'center', }}>
       <Typography 
         variant="h2" 
         sx={{ marginBottom: theme.spacing(2) }}
@@ -24,15 +29,27 @@ function UserPage() {
       >
        {user.email}
       </Typography>
-      
-      <Typography>{JSON.stringify(user, null, 2)}</Typography>
 
-      <Typography variant="body1">
+      <Avatar
+          sx={{ width: 100, height: 100, marginRight: 2 }}
+          alt="Profile Picture"
+          src="/path/to/profile-image.jpg"
+        />
+    
+      <Button
+            variant="contained"
+            className="primary on"
+            onClick={editUserinfo}
+          >
+           
+           Edit Profile
+          </Button>
+      
+      {/* <Typography>{JSON.stringify(user, null, 2)}</Typography> */}
+
+      {/* <Typography variant="body1">
         Your ID is: {user.id}
-      </Typography>
-
-      
-
+      </Typography> */}
       {/* Assuming LogOutButton can accept MUI Button props */}
       {/* <LogOutButton 
         className="primary on" 
