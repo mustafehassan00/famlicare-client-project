aea93c13fdf07556fc2e60548476ace991d6fc88
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-06-24 10:42:59 -0500

    style back arrow button on edit form

--- src/components/UserPage/EdituserPage.jsx
+++ src/components/UserPage/EdituserPage.jsx
@@ -1,108 +1,100 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Container, Typography, Button, useTheme,Avatar,Grid,TextField } from '@mui/material';
-import { useParams } from 'react-router-dom'
-import { useHistory } from "react-router-dom";
+import { Container, Typography, Button, Avatar, Grid, TextField } from '@mui/material';
+import { useParams, useHistory } from 'react-router-dom';
+import ArrowBack from '@mui/icons-material/ArrowBack';
 
-
-//fetch the data of the user that i want to edit;
-//need to know id of the user to edit 
 function EdituserPage() {
   const history = useHistory();
-
-  const theme = useTheme();
   const user = useSelector((store) => store.user);
   const params = useParams();
   const dispatch = useDispatch();
-  console.log('this is the params',params);
-  const id4userToedit = params.id
-  console.log('user to edit', id4userToedit);
-  // using this id to make a get request 
+  const id4userToedit = params.id;
+  const editUserprofile = useSelector((store) => store.editUserprofile);
 
-  //get data that's in the reducer editUser reducer
-const editUserprofile = useSelector((store) => store.editUserprofile);
-console.log('sent from reducer',editUserprofile);
-// disparch user id to saga, to get user data.
-useEffect (() => {
-    dispatch ({
-        type: "EDIT-USER-PROFILE",
-        payload: id4userToedit
-    })
-},[])
+  useEffect(() => {
+    dispatch({
+      type: "EDIT-USER-PROFILE",
+      payload: id4userToedit
+    });
+  }, [dispatch, id4userToedit]);
 
-const handleNamechange = (event) => {
-    dispatch ({
-        type: "CHANGE-CURRENT USERNAME",
-        payload: event.target.value
-    })
-}
-const handleEmailchange = (event) => {
-    dispatch ({
-        type: "CHANGE-CURRENT EMAIL",
-        payload: event.target.value
-    })
-}
-const handlePhonechange = (event) => {
-    dispatch ({
-        type: "CHANGE-CURRENT PHONE-NUMBER",
-        payload: event.target.value
-    })
-}
+  const handleNamechange = (event) => {
+    dispatch({
+      type: "CHANGE-CURRENT USERNAME",
+      payload: event.target.value
+    });
+  };
 
-const updateProfile = (event) => {
-    event.preventDefault()
-    // Sends values reducer to saga to update user profile
-    dispatch ({
-        type: "CHANGE-PROFILE-VALUES",
-        payload: editUserprofile
-    })
-}
+  const handleEmailchange = (event) => {
+    dispatch({
+      type: "CHANGE-CURRENT EMAIL",
+      payload: event.target.value
+    });
+  };
+
+  const handlePhonechange = (event) => {
+    dispatch({
+      type: "CHANGE-CURRENT PHONE-NUMBER",
+      payload: event.target.value
+    });
+  };
+
+  const updateProfile = (event) => {
+    event.preventDefault();
+    dispatch({
+      type: "CHANGE-PROFILE-VALUES",
+      payload: editUserprofile
+    });
+  };
+
   return (
     <Grid
       container
       alignItems="center"
       justifyContent="center"
       spacing={2}
-      padding={4}
+      sx={{ p: 4 }}
     >
-            <Button onClick={() => history.goBack()}> ⬅️ </Button>
+      <Button variant="outlined" color="primary" startIcon={<ArrowBack/>} onClick={() => history.goBack()}>Back</Button>
 
-      <Grid item justifyContent="Center" >
+      <Grid item justifyContent="center">
         <Avatar
           sx={{ width: 100, height: 100 }}
           alt="Profile Picture"
           src="/path/to/profile-image.jpg"
-         
         />
-        <br></br>
-        Name
+        <Typography variant="h6" component="div" sx={{ mt: 2 }}>
+          Name
+        </Typography>
         <TextField
           variant="outlined"
-           value={editUserprofile.username || ''}
-           onChange={handleNamechange}
+          value={editUserprofile.username || ''}
+          onChange={handleNamechange}
           fullWidth
           margin="normal"
         />
-          <br></br>
-        Email
-          <TextField
+        <Typography variant="h6" component="div" sx={{ mt: 2 }}>
+          Email
+        </Typography>
+        <TextField
           variant="outlined"
-           value={editUserprofile.email || ''}
-           onChange={handleEmailchange}
+          value={editUserprofile.email || ''}
+          onChange={handleEmailchange}
           fullWidth
           margin="normal"
         />
-        <br></br>
-        Phone Number
-             <TextField
+        <Typography variant="h6" component="div" sx={{ mt: 2 }}>
+          Phone Number
+        </Typography>
+        <TextField
           variant="outlined"
-           value={editUserprofile.phone_number
-             || ''}
-           onChange={handlePhonechange}
+          value={editUserprofile.phone_number || ''}
+          onChange={handlePhonechange}
           fullWidth
           margin="normal"
         />
-        <Button variant="contained" className="primary on" onClick={updateProfile}>
+        <Button variant="contained" sx={{ mt: 2 }} onClick={updateProfile}>
           Save
         </Button>
       </Grid>
