9e5599f2d7623cfe3540edd9a724d10f4afdeb0a
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-06-25 10:59:10 -0500

    adding navigation checks to ensure data only renders when it changes

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,22 +1,62 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { Container, Typography, Button, useTheme,Avatar,Grid,TextField } from '@mui/material';
-// import LogOutButton from '../LogOutButton/LogOutButton';
-import { useHistory } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Container,
+  Typography,
+  Button,
+  useTheme,
+  Avatar,
+  Grid,
+  TextField,
+} from "@mui/material";
+import { useHistory, useLocation } from "react-router-dom";
 
+// Custom hook to track previous location
+// Useful for actions that depend on navigation history, such as refetching data after navigation
+function usePreviousLocation() {
+  const location = useLocation();
+  const [prevLocation, setPrevLocation] = useState();
 
+  useEffect(() => {
+    // Update previous location whenever the current location changes
+    setPrevLocation(location);
+  }, [location]);
+
+  return prevLocation;
+}
 
 function UserPage() {
   const theme = useTheme();
-  const user = useSelector((store) => store.user);
+  const user = useSelector((store) => store.user); // Accessing user data from Redux store
   const history = useHistory();
+  const dispatch = useDispatch();
+  const prevLocation = usePreviousLocation();
 
   const editUserinfo = () => {
-    // route to the form where the user can edit their information
+    // Route to the form where the user can edit their information
+    // Ensure the route matches your routing setup
     history.push(`/update-user/${user.id}`);
-    console.log('Edit the user with the id of',user.id)
-
+    console.log("Edit the user with the id of", user.id); // Debugging: Log user ID on edit attempt
   };
+
+  useEffect(() => {
+    // Fetch user data on component mount
+    // Make sure the action type "FETCH_USER" is handled in your Redux saga or reducer
+    dispatch({ type: "FETCH_USER" });
+  }, [dispatch]);
+
+  // Refetch user data when navigating back to the UserPage after an update
+  // This relies on changes in the history object to trigger a refetch
+  useEffect(() => {
+    // Check if the user navigated from `/update-user/${user.id}` to `/user`
+    // Adjust the paths as necessary if routing structure changes
+    if (
+      prevLocation?.pathname === `/update-user/${user.id}` &&
+      location.pathname === "/user"
+    ) {
+      dispatch({ type: "FETCH_USER" }); // Refetch user data
+    }
+  }, [dispatch, location, prevLocation, user.id]);
 
   return (
     <>
@@ -34,59 +74,43 @@ function UserPage() {
             <Avatar
               sx={{ width: 100, height: 100 }}
               alt="Profile Picture"
-              src="/path/to/profile-image.jpg"
+              src="/path/to/profile-image.jpg" // Ensure the path is correct for profile images
             />
           </Grid>
           <Grid item>
             <Typography variant="h2" sx={{ marginBottom: theme.spacing(1) }}>
-              {user.username}
+              {user.username} {/* Display the username */}
               <br></br>
-              {user.id}
-
+              {user.id} {/* Display the user ID */}
             </Typography>
             <Typography variant="h3" sx={{ marginBottom: theme.spacing(1) }}>
-              {user.email}
+              {user.email} {/* Display the user email */}
             </Typography>
             <Button
               variant="contained"
               className="primary on"
               onClick={editUserinfo}
             >
-              Edit Profile
+              Edit Profile {/* Button to trigger user info edit */}
             </Button>
           </Grid>
         </Grid>
       </Container>
       <TextField
-          variant="outlined"
-          placeholder='Privacy Policy {coming soon}'
-          disabled
-           
-          fullWidth
-          margin="normal"
-        />
-             <TextField
-          variant="outlined"
-          placeholder='Change Password {coming soon}'
-          disabled
-          fullWidth
-          margin="normal"
-        />
-        
-
-
-      {/* <Typography>{JSON.stringify(user, null, 2)}</Typography> */}
-
-      {/* <Typography variant="body1">
-        Your ID is: {user.id}
-      </Typography> */}
-      {/* Assuming LogOutButton can accept MUI Button props */}
-      {/* <LogOutButton 
-        className="primary on" 
-        component={Button} 
-        sx={{ marginTop: theme.spacing(2) }}
-      /> */}
-    </>
+        variant="outlined"
+        placeholder="Privacy Policy {coming soon}"
+        disabled // Placeholder for future features
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        variant="outlined"
+        placeholder="Change Password {coming soon}"
+        disabled // Placeholder for future features
+        fullWidth
+        margin="normal"
+      />
+      </>
   );
 }
 
