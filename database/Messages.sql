ca3bb4b4ccdcc84e3a979f34cc2691964d0258fd
Mustafe Hassan <155122726+mustafehassan00@users.noreply.github.com>
2024-06-26 14:50:02 -0500

    Update Messages.sql

--- database/Messages.sql
+++ database/Messages.sql
@@ -1,6 +1,7 @@
 CREATE TABLE messages(
-    id SERIAL PRIMARY KEY,
-    loved_one_id INTEGER REFERENCES loved_ones(id),
-    user_id INTEGER REFERENCES "user"(id),
-    message_text TEXT
+  id SERIAL PRIMARY KEY,
+  loved_one_id INTEGER REFERENCES loved_ones(id),
+  user_id INTEGER REFERENCES "user"(id),
+  message_text TEXT,
+  msg_sent_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
